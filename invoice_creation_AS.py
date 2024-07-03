a19c09f73de6bc1a6de20d2577f39b4df9ed34db
HectorHQ <109182400+HectorHQ@users.noreply.github.com>
2024-07-03 17:09:59 -0600

    Update invoice_creation_AS.py

--- invoice_creation_AS.py
+++ invoice_creation_AS.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
                 inv_id = i['InvoiceID']
 
                 data = invoice_creation_AS(headers,Org_id,dueDate,amount=amount,toogle=toggle,notes=notes,invoice_id=inv_id)
-                st.text(data.content)
+                
                 st.text(data.text)
                 data = data.json()
                 st.markdown('---')
